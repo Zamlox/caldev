13cc085df9506c99689de65510bded250c0f89be
Iosif Haidu <iosif.haidu@gmail.com>
2020-12-02 16:34:11 +0000

    Add GUI to API

---
+++ src/modules/gui/glfwgui.h
@@ -0,0 +1,19 @@
+/**
+ * Author: 		Iosif Haidu
+ * Description: GUI engine implementation based on Glfw 
+ *
+ * Copyright 2020 Iosif Haidu - All rights reserved.
+ */
+
+#include "igui.h"
+
+namespace GUI
+{
+
+class GlfwGUI : public IGui
+{
+public:
+
+};
+
+} // namespace GUI
