de44f7599745898eedad2b33b70ba99d886f4f06
Iosif Haidu <iosif.haidu@gmail.com>
2020-12-13 07:08:18 +0000

    Add widget factory interface

---
+++ src/internal/gui/iwidgetfactory.h
@@ -0,0 +1,34 @@
+/**
+ * Author: 		Iosif Haidu
+ * Description: Interface for creating widgets
+ *
+ * Copyright 2020 Iosif Haidu - All rights reserved.
+ */
+
+#ifndef __GUI_IWIDGETFACTORY_H__
+#define __GUI_IWIDGETFACTORY_H__
+
+#include "iwidget.h"
+#include "iwindow.h"
+#include "imgui/common.h"
+
+namespace GUI
+{
+
+class IWidgetFactory
+{
+public:
+    virtual ~IWidgetFactory() = default;
+    /**
+     * Create a window.
+     * @param  {char*} titleP       : title of window (can be UTF-8 if backend allows)
+     * @param  {WindowsFlags} flags : flags for creating window 
+     * @param  {Font*} pFontP       : font to be used in window
+     * @return {IWindow*}           : new window if succeeded or nullptr if failed
+     */
+    virtual IWindow* createWindow(const char* titleP, WindowsFlags flags, Font* pFontP) = 0;    
+};
+
+} // namespace GUI
+
+#endif // __GUI_IWIDGETFACTORY_H__
