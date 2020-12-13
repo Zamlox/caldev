861afcf0bd07141c9bbb2c9d73f808b0b965dad6
Iosif Haidu <iosif.haidu@gmail.com>
2020-12-13 05:05:14 +0000

    Add GUI engine in separate thread

--- src/modules/gui/opengl.h
+++ src/modules/gui/opengl.h
@@ -6,14 +6,37 @@
  */
 
 #include "igui.h"
+#include "thread.h"
 
 namespace GUI
 {
 
+/**
+ * GUI engine implementing IGui
+ */
 class OpenGL : public IGui
 {
 public:
+    OpenGL();
+    /** see IGui::start() */
+    bool start() override;
+    /** see IGui::stop() */
+    bool stop() override;
 
+private:
+    /**
+     * Code to run before returning back to caller after thread starts
+     * @param  {void*} pParamP : pass 'this' to have access to internal data
+     */
+    static void* initGuiEngine(void* pParamP);
+    /**
+     * Code to run for GUI engine 
+     * @param  {void*} pParamP : pass 'this' to have access to internal data
+     */
+    static void* guiEngine(void*pParamP);
+
+    /** Separate thread to run GUI engine */
+    Os::Thread threadM;
 };
 
 } // namespace GUI
