646f4403913289b9a2874534aada781e3b2e2778
Iosif Haidu <iosif.haidu@gmail.com>
2021-01-02 14:37:08 +0200

    Add rebol2 bindings

---
+++ src/tests/bindings/rebol2/test-gui.r
@@ -0,0 +1,48 @@
+REBOL [
+    Title:      "GUI engine tests"
+    Purpose:    "Unit tests for GUI engine"
+    file:       %test-gui.r
+    author:     "Iosif Haidu"
+    email:      [iosif.haidu@gmail.com]
+    date:       30-12-2020
+    version:    0.0.1
+    needs:      [
+        %lib.r
+    ]
+    History: [
+        0.0.1 [31-12-2020 "Started"]
+    ]
+    Category:   [tests library graphics]
+    library:    [
+        level:          'intermediate
+        platform:       ['win 'linux 'macOS]
+        type:           [test]
+        domain:         [tests graphics external-library]
+        tested-under:   [
+            View 2.7.8.3.1
+            View 2.7.8.4.3
+            View 2.7.8.2.5
+        ]
+        support:        none
+        license:        'MIT
+    ]
+    Notes: {
+        rebosdk library can be found at: https://github.com/Zamlox/rebosdk
+    }
+]
+
+do %../../../bindings/rebol2/lib.r
+
+context [
+    test-main-window: does [
+        init-gui GUIType/OpenGL
+        either (create-main-window "Rebol2 Main Window" 250 250 400 400 10100 1) > 0 [
+            print "Wait for 10 seconds..."
+            wait 10
+        ][ alert "Something wrong happened while creating main window !" ]
+        stop-gui
+    ]
+
+    test-main-window
+]
+
