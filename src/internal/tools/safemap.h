ba16baa5c9c918a09a7b62c784c4c65ca9f77024
Iosif Haidu <iosif.haidu@gmail.com>
2021-01-27 23:21:13 +0200

    Enhance safemap

--- src/internal/tools/safemap.h
+++ src/internal/tools/safemap.h
@@ -10,6 +10,7 @@
 
 #include "internal/os/mutex.h"
 #include <map>
+#include <functional>
 #include <gsl/pointers.h>
 
 namespace Tools
@@ -25,6 +26,7 @@ class SafeMap
 {
 private:
     using Map = std::map<TKEY, TVALUE>;
+    using ProcessFunc = std::function<void(TVALUE&)>;
 
 public:
     SafeMap()
@@ -44,16 +46,44 @@ public:
     {
         bool found{false};
         pSyncExternalM->lock();
-        if (typename Map::const_iterator it = mapM.find(rKeyP); it != mapM.end())
+        found = getElem(rKeyP, rValueP);
+        pSyncExternalM->unlock();
+        return found;
+    }
+    void process(TKEY const rKeyP, ProcessFunc funcP)
+    {
+        bool found{false};
+        TVALUE elem;
+        pSyncExternalM->lock();
+        if ((found = getElem(rKeyP, elem)))
         {
-            rValueP = it->second;
-            found = true;
+            funcP(elem);
         }
         pSyncExternalM->unlock();
         return found;
     }
+    void processAll(ProcessFunc funcP)
+    {
+        pSyncExternalM->lock();
+        for(auto &rElem : mapM)
+        {
+            funcP(rElem.second);
+        }
+        pSyncExternalM->unlock();
+    }
 
 private:
+    bool getElem(TKEY const rKeyP, TVALUE& rValueP) const
+    {
+        bool found{false};
+        if (typename Map::const_iterator it = mapM.find(rKeyP); it != mapM.end())
+        {
+            found = true;
+            rValueP = it->second;
+        }
+        return found;
+    }
+
     Map mapM;
     Os::Mutex syncMapM;
     Os::Mutex* pSyncExternalM;
