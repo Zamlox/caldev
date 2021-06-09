b1dac80df4c554dd867a8c4097e46aac89c3baaf
Iosif Haidu <iosif.haidu@gmail.com>
2021-06-10 01:15:51 +0300

    Update usage of WidgetAttributes

--- src/internal/gui/widgets/radio.cpp
+++ src/internal/gui/widgets/radio.cpp
@@ -59,6 +59,7 @@ void RadioButton::beginRender()
             , alignM
             , ImRect(xM, yM, xM + widthM, yM + heightM)
             , (isWrapM) ? widthM : 0.0
+            , ImRect{0, 0, 0, 0}
         );
         
         if (pFontM) ImGui::PushFont(pFontM);
