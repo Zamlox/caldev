c0177a14361b150f3a6d312b759924ead500611f
Iosif Haidu <iosif.haidu@gmail.com>
2021-12-01 22:33:43 +0200

    Layout begin

---
+++ src/internal/gui/layout/format/alignment.h
@@ -0,0 +1,25 @@
+/*
+ * Created by Iosif Haidu on 29/11/2021.
+ * Copyright (c) 2021 Iosif Haidu. All rights reserved.
+ * Description: Format attributes for layout cell
+ */
+
+#pragma once
+
+namespace GUI::Layout {
+
+/** Alignment types for horizontal direction */
+enum class HorizAlign {
+    LEFT,
+    CENTER,
+    RIGHT
+};
+
+/** Alignment types for vertical direction */
+enum class VertAlign {
+    TOP,
+    MIDDLE,
+    BOTTOM
+};
+
+} // namespace Gui::Layout
